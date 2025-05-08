b911ff1fb27888ec59546ac6b049d5fca2e38751
cseMisbah <mijbahsakib@gmial.com>
2025-05-08 14:06:58 +0600

    updated

--- problem-19.js
+++ problem-19.js
@@ -1,5 +1,5 @@
 //Promise
-
+/*
 const myPromise = new Promise(function (myResolve, myReject) {
   setTimeout(() => {
     myResolve("I love you");
@@ -9,3 +9,12 @@ const myPromise = new Promise(function (myResolve, myReject) {
 myPromise.then(function (value) {
   document.getElementById("demo").innerHTML = value;
 });
+*/
+const numbers = [4, 9, 16, 25, 29];
+let first = numbers.find(myFunction);
+
+document.getElementById("demo").innerHTML = "First number over 18 is " + first;
+
+function myFunction(value, index, array) {
+  return value > 18;
+}
