c22006d79b743a0977c7054d29fb470c908e21b0
cseMisbah <mijbahsakib@gmial.com>
2025-05-12 19:48:00 +0600

    solved problem-28.js

---
+++ problem-28.js
@@ -0,0 +1,25 @@
+//create a basic digital clock
+
+//const watchElement = document.querySelector(".watch");
+//const watchButton = document.querySelector("#watch-btn")
+
+const watchElement = document.querySelector(".watch");
+
+const watchButton = document.querySelector("#watch-btn");
+
+const updateClock = () => {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  const timeString = `${hours}:${minutes}:${seconds}`;
+  watchElement.textContent = timeString;
+};
+
+const intervalId = setInterval(updateClock, 1000);
+
+const stopWatch = () => {
+  clearInterval(intervalId);
+};
+
+watchButton?.addEventListener("click", stopWatch);
